f9f4b611368f91abd96fc6f65ef56b7bf724147a
gfhdthev <akkglow@tutamail.com>
2025-03-03 13:50:53 +0300

    second

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from oktagon import Oktagon
+from oktagon import Oktagon #импортируем класс
 
-def main():
-    first = Oktagon(8)
-    first.plo()
+def main(): #создаем функцию 
+    first = Oktagon(8) #добавляем объект
+    first.plo() #используем все методы
     first.per()
     first.opis_okr()
     first.vpis_okr()
     first.ris()
 
-if __name__ == '__main__':
-    main()
+if __name__ == '__main__': #проверяем на прямой запуск
+    main() #запускаем функцию
